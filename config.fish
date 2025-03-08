fe37cf22f689bf7439fa90e41f044f78b8a8a635
Doridian <git@doridian.net>
2025-03-08 12:27:43 -0800

    Apply fish formatting

--- config.fish
+++ config.fish
@@ -3,41 +3,46 @@ set _fconfd_loader_debug 0
 set _fconfd_root_dir "$(dirname "$(realpath "$(status --current-filename)")")/fish.conf.d"
 
 function _fconfd_source_file -a file
-	if test "$_fconfd_loader_debug" -eq 1;
-		echo "Loading file $file"
-	end
-	source "$file"
+    if test "$_fconfd_loader_debug" -eq 1
+
+        echo "Loading file $file"
+    end
+    source "$file"
 end
 
 function _fconfd_source_dir -a dir
-	if test "$_fconfd_loader_debug" -eq 1;
-		echo "Loading dir $_fconfd_root_dir/$dir"
-	end
-	for file in "$_fconfd_root_dir/$dir"/*.fish
-		_fconfd_source_file "$file"
-	end
+    if test "$_fconfd_loader_debug" -eq 1
+
+        echo "Loading dir $_fconfd_root_dir/$dir"
+    end
+    for file in "$_fconfd_root_dir/$dir"/*.fish
+        _fconfd_source_file "$file"
+    end
 end
 
 function _fconfd_source_config_root -a dir
-	if test "$_fconfd_loader_debug" -eq 1;
-		echo "Loading root $_fconfd_root_dir/$dir"
-	end
+    if test "$_fconfd_loader_debug" -eq 1
 
-	_fconfd_source_dir "$dir"
-	if test -z "$SSH_CLIENT";
-		_fconfd_source_dir "$dir/local"
-	else
-		_fconfd_source_dir "$dir/remote"
-	end
+        echo "Loading root $_fconfd_root_dir/$dir"
+    end
+
+    _fconfd_source_dir "$dir"
+    if test -z "$SSH_CLIENT"
+
+        _fconfd_source_dir "$dir/local"
+    else
+        _fconfd_source_dir "$dir/remote"
+    end
 end
 
 function fconfd_reload
-	_fconfd_source_config_root always
-	if status is-interactive;
-		_fconfd_source_config_root interactive
-	else
-		_fconfd_source_config_root non-interactive
-	end
+    _fconfd_source_config_root always
+    if status is-interactive
+
+        _fconfd_source_config_root interactive
+    else
+        _fconfd_source_config_root non-interactive
+    end
 end
 
 fconfd_reload
--- install.fish
+++ install.fish
@@ -1,14 +1,14 @@
 #!/usr/bin/env fish
 
 function symlink_file -a src dst
-	set dst_dir "$(realpath "$(dirname "$dst")")"
+    set dst_dir "$(realpath "$(dirname "$dst")")"
 
-	set src "$(realpath "$src")"
-	set src_rel "$(realpath -s --relative-to="$dst_dir" "$src")"
+    set src "$(realpath "$src")"
+    set src_rel "$(realpath -s --relative-to="$dst_dir" "$src")"
 
-	mkdir -p "$dst_dir"
-	echo "Symlinking $src_rel to $dst"
-	ln -sf "$src_rel" "$dst"
+    mkdir -p "$dst_dir"
+    echo "Symlinking $src_rel to $dst"
+    ln -sf "$src_rel" "$dst"
 end
 
 symlink_file gitconfig ~/.gitconfig
