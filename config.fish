eef2b38868ad99499319837ce358b2a83f28cde7
Doridian <git@doridian.net>
2025-03-08 12:25:48 -0800

    underscore

--- config.fish
+++ config.fish
@@ -1,41 +1,43 @@
-set fconfd_loader_debug 0
+set _fconfd_loader_debug 0
 
-set fconfd_root_dir "$(dirname "$(realpath "$(status --current-filename)")")/fish.conf.d"
+set _fconfd_root_dir "$(dirname "$(realpath "$(status --current-filename)")")/fish.conf.d"
 
-alias fconfd_is_loader_debug='test $fconfd_loader_debug -eq 1'
-
-function fconfd_source_file -a file
-	if fconfd_is_loader_debug;
+function _fconfd_source_file -a file
+	if test "$_fconfd_loader_debug" -eq 1;
 		echo "Loading file $file"
 	end
 	source "$file"
 end
 
-function fconfd_source_dir -a dir
-	if fconfd_is_loader_debug;
-		echo "Loading dir $fconfd_root_dir/$dir"
+function _fconfd_source_dir -a dir
+	if test "$_fconfd_loader_debug" -eq 1;
+		echo "Loading dir $_fconfd_root_dir/$dir"
 	end
-	for file in "$fconfd_root_dir/$dir"/*.fish
-		fconfd_source_file "$file"
+	for file in "$_fconfd_root_dir/$dir"/*.fish
+		_fconfd_source_file "$file"
 	end
 end
 
-function fconfd_source_config_root -a dir
-	if fconfd_is_loader_debug;
-		echo "Loading root $fconfd_root_dir/$dir"
+function _fconfd_source_config_root -a dir
+	if test "$_fconfd_loader_debug" -eq 1;
+		echo "Loading root $_fconfd_root_dir/$dir"
 	end
 
-	fconfd_source_dir "$dir"
+	_fconfd_source_dir "$dir"
 	if test -z "$SSH_CLIENT";
-		fconfd_source_dir "$dir/local"
+		_fconfd_source_dir "$dir/local"
 	else
-		fconfd_source_dir "$dir/remote"
+		_fconfd_source_dir "$dir/remote"
 	end
 end
 
-fconfd_source_config_root always
-if status is-interactive;
-	fconfd_source_config_root interactive
-else
-	fconfd_source_config_root non-interactive
+function fconfd_reload
+	_fconfd_source_config_root always
+	if status is-interactive;
+		_fconfd_source_config_root interactive
+	else
+		_fconfd_source_config_root non-interactive
+	end
 end
+
+fconfd_reload
