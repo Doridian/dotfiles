0c9592a358049cc186de66179f75e7bc400e2e6a
Doridian <git@doridian.net>
2025-03-09 10:38:30 -0700

    updater now git pulls

--- update.fish
+++ update.fish
@@ -1,9 +1,17 @@
 #!/usr/bin/env fish
 
-# curl -fsL https://raw.githubusercontent.com/Doridian/dotfiles/refs/heads/main/install.fish | fish
+set -l _stage2 $argv[1]
 
 set -l _cur_filename "$(status --current-filename)"
 cd "$(dirname "$_cur_filename")"
+set -e _cur_filename
+
+if test "$_stage2" != gitpulldone
+    git pull
+    ./update.fish gitpulldone
+    exit 0
+end
+# TRY NOT TO EDIT ABOVE THIS LINE, IT MAKES AUTOUPDATES HARDER!
 
 function _fconfd_symlink_file -a src dst
     set -l dst_dir "$(realpath "$(dirname "$dst")")"
@@ -19,3 +27,4 @@ end
 _fconfd_symlink_file gitconfig ~/.gitconfig
 _fconfd_symlink_file ssh.config ~/.ssh/config
 _fconfd_symlink_file config.fish ~/.config/fish/config.fish
+exit 0
