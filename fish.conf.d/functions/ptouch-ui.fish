103f0b675a8eef799a5522ae1156bde5085f109a
Doridian <git@doridian.net>
2025-03-09 18:57:36 -0700

    Add help

--- fish.conf.d/functions/ptouch-ui.fish
+++ fish.conf.d/functions/ptouch-ui.fish
@@ -11,12 +11,53 @@ function _run_ptouch_print -a font_size text writepng
     ptouch-print $args 2>/dev/null
 end
 
+function _ptouch_cmd_info -a cmd info
+    set_color blue
+    echo -n "    !$cmd: "
+    set_color yellow
+    echo $info
+    set_color normal
+end
+
+function _ptouch_ui_help
+    echo ' '
+
+    set_color green
+    echo '=== ptouch-ui usage help ==='
+    set_color normal
+    echo ' '
+
+    echo 'ptouch-ui accepts two kinds of input:'
+    echo -n '    1. '; set_color red; echo -n 'Text'; set_color normal; echo ' to print'
+    echo -n '    2. '; set_color red; echo -n 'Commands'; set_color normal; echo ' (prefixed with !)'
+    echo 'Lines starting with ! are treated as commands'
+    echo 'Any other line changes the text to print on the label to the current input'
+
+    echo ' '
+
+    set_color green
+    echo 'Commands:'
+    set_color normal
+    _ptouch_cmd_info 'info' 'Print information about the label printer'
+    _ptouch_cmd_info 'print' 'Print the current text'
+    _ptouch_cmd_info 'help' 'Print this help message'
+    _ptouch_cmd_info 'exit' 'Exit the program'
+    _ptouch_cmd_info '<number>' 'Set the font size to <number> (0 for automatic)'
+
+    echo ' '
+
+end
+
 function ptouch-ui
     set -f font_size 0
     set -f text 'Meow :3'
 
+    set_color blue
+    echo 'Use !help for help'
+    set_color normal
+
     while read -L -f input
-        if test (string sub --length 1 "$input") = '='
+        if test (string sub --length 1 "$input") = '!'
             set -f input (string sub --start 2 "$input")
             if test "$input" = 'info'
                 set_color blue
@@ -24,16 +65,25 @@ function ptouch-ui
                 set_color normal
             else if test "$input" = 'print'
                 _run_ptouch_print $font_size $text
+            else if test "$input" = 'help'
+                _ptouch_ui_help
+                continue
             else if test "$input" = 'exit'
                 break
-            else if string match -r -- '^[0-9]+$' "$input"
+            else if test "$input" = 'quit'
+                break
+            else if test "$input" = 'bye'
+                break
+            else if string match -r -- '^[0-9]+$' "$input" >/dev/null
                 set -f font_size $input
             else
                 set_color red
                 echo "Invalid input: $input"
                 set_color normal
+                _ptouch_ui_help
+                continue
             end
-        else
+        else if ! test -z "$input"
             set -f text "$input"
         end
 
