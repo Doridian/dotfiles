46908e0fd064d206f91d40d9516f38138db97427
Doridian <git@doridian.net>
2025-03-13 12:54:00 -0700

    rename

--- fish.conf.d/functions/youtube-download.fish
+++
@@ -1,5 +0,0 @@
-function youtube-download
-    yt-dlp --no-post-overwrites --download-archive "$_youtube_download_dir/yt-dlp-archive.txt" --merge-output-format mkv --sub-langs all,-live_chat --embed-subs --embed-metadata --cookies-from-browser firefox -o "$_youtube_download_dir/"'%(channel)s/%(title)s [%(id)s].%(ext)s' $argv
-end
-
-# Use --batch-file for batches
