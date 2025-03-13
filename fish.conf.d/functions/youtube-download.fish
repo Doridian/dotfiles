cda7079c86f21a9f154822533588618bb0bef7a2
Doridian <git@doridian.net>
2025-03-13 12:52:35 -0700

    dont hardcode

--- fish.conf.d/functions/youtube-download.fish
+++ fish.conf.d/functions/youtube-download.fish
@@ -1,5 +1,5 @@
 function youtube-download
-    yt-dlp --no-post-overwrites --download-archive ~/smb4k/BENGALFOX/share/archive/YouTube/yt-dlp-archive.txt --merge-output-format mkv --sub-langs all,-live_chat --embed-subs --embed-metadata --cookies-from-browser firefox -o ~/smb4k/BENGALFOX/share/archive/YouTube/'%(channel)s/%(title)s [%(id)s].%(ext)s' $argv
+    yt-dlp --no-post-overwrites --download-archive "$_youtube_download_dir/yt-dlp-archive.txt" --merge-output-format mkv --sub-langs all,-live_chat --embed-subs --embed-metadata --cookies-from-browser firefox -o "$_youtube_download_dir/"'%(channel)s/%(title)s [%(id)s].%(ext)s' $argv
 end
 
 # Use --batch-file for batches
