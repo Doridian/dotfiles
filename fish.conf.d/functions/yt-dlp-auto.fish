487cbbbc104013ef4c33575763725c06a1396fab
Doridian <git@doridian.net>
2025-03-13 18:05:29 -0700

    profile

--- fish.conf.d/functions/yt-dlp-auto.fish
+++ fish.conf.d/functions/yt-dlp-auto.fish
@@ -1,5 +1,5 @@
 function yt-dlp-auto
-    yt-dlp --no-post-overwrites --download-archive "$_ytdlp_auto_dir/yt-dlp-archive.txt" --merge-output-format mkv --sub-langs all,-live_chat --embed-subs --embed-metadata --cookies-from-browser firefox -o "$_ytdlp_auto_dir/"'%(channel)s/%(title)s [%(id)s].%(ext)s' $argv
+    yt-dlp --no-post-overwrites --download-archive "$_ytdlp_auto_dir/yt-dlp-archive.txt" --merge-output-format mkv --sub-langs all,-live_chat --embed-subs --embed-metadata --cookies-from-browser "$_ytdlp_browser" -o "$_ytdlp_auto_dir/"'%(channel)s/%(title)s [%(id)s].%(ext)s' $argv
 end
 
 # Use --batch-file for batches
