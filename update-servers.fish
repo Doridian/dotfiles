78175544f121fc1ba670f07becad2aa351cceaf2
Doridian <git@doridian.net>
2025-03-14 10:51:45 -0700

    fixups

--- update-servers.fish
+++ update-servers.fish
@@ -2,26 +2,28 @@
 
 cd (dirname (status --current-filename))
 
-function _server_init -a gitremote server
-	if git push $gitremote main
-		return 0
-	end
-
-	ssh $server -- /bin/sh -c "'mkdir -p ~/dotfiles && git -C ~/dotfiles init && git -C ~/dotfiles receive.denyCurrentBranch ignore'"
-	or return 1
-
-	git push $gitremote main
-end
-
-for server in (cat ./machines/servers.txt)
-	#set -l remote_home (ssh "$server" -- echo '$HOME')
-        #echo "Home = $remote_home"
-
+function _server_update -a server
 	set -l gitremote "server-$server"
 
 	git remote add $gitremote "$server:~/dotfiles"
 	or git remote set-url $gitremote "$server:~/dotfiles"
 
-	_server_init $gitremote $server
+	git push $gitremote main
+	ssh $server -- /bin/sh -c "'cd ~/dotfiles && git reset --hard'"
+end
+
+function _server_init -a server
+	if _server_update $server
+		return 0
+	end
+
+	ssh $server -- /bin/sh -c "'rm -rf ~/dotfiles && mkdir -p ~/dotfiles && cd ~/dotfiles && git init && git config receive.denyCurrentBranch ignore'"
+	or return 1
+
+	_server_update $server
+end
+
+for server in (cat ./machines/servers.txt)
+	_server_init $server
 end
 
