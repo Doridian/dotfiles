c4c10151bcfd666952bf28a8bf91de0e44e2c61c
Doridian <git@doridian.net>
2025-03-15 10:13:22 -0700

    Force batch file

--- fish.conf.d/functions/yt-dlp-auto.fish
+++ fish.conf.d/functions/yt-dlp-auto.fish
@@ -1,5 +1,3 @@
 function yt-dlp-auto
-    yt-dlp --no-post-overwrites --download-archive ./yt-dlp-archive.txt --merge-output-format mkv --sub-langs all,-live_chat --embed-subs --embed-metadata --cookies-from-browser "$_ytdlp_browser" -o ./'%(channel)s/%(title)s [%(id)s].%(ext)s' $argv
+    yt-dlp --no-post-overwrites --download-archive ./yt-dlp-archive.txt --merge-output-format mkv --sub-langs all,-live_chat --embed-subs --embed-metadata --cookies-from-browser "$_ytdlp_browser" -o ./'%(channel)s/%(title)s [%(id)s].%(ext)s' --batch-file ./downloads.txt
 end
-
-# Use --batch-file for batches
