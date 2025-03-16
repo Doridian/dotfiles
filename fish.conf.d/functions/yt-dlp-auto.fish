742745251049e3f2de92a091d81d5040b0314ca0
Doridian <git@doridian.net>
2025-03-15 23:02:18 -0700

    uploader

--- fish.conf.d/functions/yt-dlp-auto.fish
+++ fish.conf.d/functions/yt-dlp-auto.fish
@@ -1,3 +1,3 @@
 function yt-dlp-auto
-    yt-dlp --no-post-overwrites --download-archive ./yt-dlp-archive.txt --merge-output-format mkv --sub-langs all,-live_chat --embed-subs --embed-metadata --cookies-from-browser "$_ytdlp_browser" -o ./'%(artist)s/%(title)s [%(id)s].%(ext)s' --batch-file ./downloads.txt
+    yt-dlp --no-post-overwrites --download-archive ./yt-dlp-archive.txt --merge-output-format mkv --sub-langs all,-live_chat --embed-subs --embed-metadata --cookies-from-browser "$_ytdlp_browser" -o ./'%(uploader)s/%(title)s [%(id)s].%(ext)s' --batch-file ./downloads.txt
 end
