47ee92b5eab758117b9633ca38f2751bde588b6a
Doridian <git@doridian.net>
2025-05-06 14:51:44 -0700

    dsp recover simple

---
+++ fish.conf.d/functions/recover-dsp.fish
@@ -0,0 +1,8 @@
+function recover-dsp -a snapshot
+    # TODO: sudo /opt/endpoint/restic.sh /opt/backup/restic.sh mount /mnt/backup
+    set -l _fullpath '/home/doridian/.local/share/Steam/steamapps/compatdata/1366540/pfx/drive_c/users/steamuser/'
+    set -l _backuproot "/mnt/backup/snapshots/$snapshot"
+    # TODO: Pick a snapshot UI for user
+    sudo rsync --delete -avogXAE "$_backuproot/$_fullpath" "$_fullpath"
+    # TODO: sudo umount /mnt/backup
+end
