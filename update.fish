d5e935bd52485d7132e2399f84099377fffc42dc
Doridian <git@doridian.net>
2025-05-22 23:07:51 -0700

    modprobed

--- update.fish
+++ update.fish
@@ -26,12 +26,15 @@ function _dotfiles_symlink_file -a src dst
     ln -Tsf "$src_rel" "$dst"
 end
 
+_dotfiles_symlink_file config.fish ~/.config/fish/config.fish
+
 _dotfiles_symlink_file "user/gitconfig-$_dotfiles_mode" ~/.gitconfig
 _dotfiles_symlink_file user/ssh.config ~/.ssh/config
 _dotfiles_symlink_file user/ssh.authorized_keys ~/.ssh/authorized_keys
-_dotfiles_symlink_file config.fish ~/.config/fish/config.fish
+_dotfiles_symlink_file user/modprobed-db.conf ~/.config/modprobed-db.conf
 _dotfiles_symlink_file user/wezterm.lua ~/.wezterm.lua
 _dotfiles_symlink_file user/screenrc ~/.screenrc
 _dotfiles_symlink_dir user/pipewire.conf.d ~/.config/pipewire/pipewire.conf.d
 _dotfiles_symlink_dir user/environment.d ~/.config/environment.d
+
 exit 0
