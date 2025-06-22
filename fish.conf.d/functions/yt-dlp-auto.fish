57b7f22db8c10130440a936b63e6f85f2acbe511
Doridian <git@doridian.net>
2025-06-22 15:48:42 -0700

    Store playlists in m3u8

--- fish.conf.d/functions/yt-dlp-auto.fish
+++ fish.conf.d/functions/yt-dlp-auto.fish
@@ -3,6 +3,29 @@ function _yt-dlp-one
     yt-dlp --no-post-overwrites --download-archive ./yt-dlp-archive.txt --merge-output-format mkv --sub-langs all,-live_chat --embed-subs --embed-metadata --cookies-from-browser "$_ytdlp_browser" -o ./'%(uploader)s/%(title)s [%(id)s].%(ext)s' $argv
 end
 
+function _yt-dlp-youtube-m3u8 -a playlist
+    echo "Invoking yt-dlp for m3u8 playlist: $playlist"
+
+    set -f playlist_data (yt-dlp --flat-playlist --dump-single-json "$playlist")
+
+    set -f playlist_id (echo "$playlist_data" | jq -r '.id')
+    set -f playlist_title (echo "$playlist_data" | jq -r '.title' | sed 's~/~⧸~g')
+    set -f playlist_video_ids (echo "$playlist_data" | jq -r '.entries[].id')
+
+    set -f playlist_file "$playlist_title [$playlist_id].m3u8"
+
+    set -f folder '.'
+    for videoid in $playlist_video_ids
+        set -f filename (find "$folder" -name "*\\[$videoid\\]*" -print -quit)
+        if test "$folder" = '.'
+            set -f folder (dirname $filename)
+            echo '#EXTM3U' > "$folder/$playlist_file"
+        end
+
+        basename "$filename" >> "$folder/$playlist_file"
+    end
+end
+
 function yt-dlp-auto
     set -f tmpfile (mktemp)
     set -f tmpfile_used 0
@@ -23,6 +46,10 @@ function yt-dlp-auto
         end
 
         _yt-dlp-one $line
+
+        if string match -q -- '*youtube.com/list?list=*' $line
+            _yt-dlp-youtube-m3u8 $line
+        end
 
         echo 'Done, sleeping for 60 seconds...'
         sleep 60s
