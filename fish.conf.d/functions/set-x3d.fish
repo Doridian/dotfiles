224ab2c31a0ca6f2895fd39999fae6fb08ff8910
Doridian <git@doridian.net>
2025-08-01 23:39:43 -0700

    new path

--- fish.conf.d/functions/set-x3d.fish
+++ fish.conf.d/functions/set-x3d.fish
@@ -1,5 +1,5 @@
 # $mode: cache, frequency
 function set-x3d -a mode
     echo -n 'Setting X3D mode to '
-    echo "$mode" | sudo tee /sys/module/amd_3d_vcache/drivers/platform:amd_x3d_vcache/*/amd_x3d_mode
+    echo "$mode" | sudo tee /sys/bus/platform/drivers/amd_x3d_vcache/*/amd_x3d_mode
 end
