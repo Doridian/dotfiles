b415cc0abc5dd88e67fbba31d34bd2bb8b403ab7
Doridian <git@doridian.net>
2025-08-14 15:09:37 -0700

    setup

--- system/secureboot-newkey.sh
+++ system/secureboot-newkey.sh
@@ -5,6 +5,8 @@ set -x
 # cp initcpio/post/00-uki-sbsign /etc/initcpio/post/
 # pacman -S sbsigntools
 
+# systemd-cryptenroll /dev/md0 --wipe-slot=tpm2 --tpm2-device=auto --tpm2-pcrs=0+7+14
+
 mkdir -p /etc/pki/sbsign
 chmod 700 /etc/pki/sbsign
 
