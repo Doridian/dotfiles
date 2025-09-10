479130d9255cd7fb097e4ac499a689bae42d3624
Doridian <git@doridian.net>
2025-09-10 12:28:01 -0700

    Add unused script

--- fish.conf.d/functions/foxdenaur-unused.fish
+++ fish.conf.d/functions/foxdenaur-unused.fish
@@ -1,7 +1,3 @@
 function foxdenaur-unused
-    function __foxdenaur-unused-filter
-        cut -d' ' -f1 | grep -v '^\\s' | grep .
-    end
-
     pacman -Ss | grep '^foxdenaur/' | grep -vF '[installed]' | cut -d/ -f2 | cut -d' ' -f1 | grep -v '[-]debug$'
 end
