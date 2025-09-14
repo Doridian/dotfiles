a98a0be111b114918260592f2183b94483d9eff1
Doridian <git@doridian.net>
2025-09-14 12:21:05 -0700

    /opt/backup

--- root-install.sh
+++ root-install.sh
@@ -8,3 +8,11 @@ pacman -S --needed --noconfirm git fish rsync diff
 mkdir -p /opt/dotfiles
 
 rsync --delete -av ./system/opt/dotfiles/ /opt/dotfiles/
+cd /opt
+if [ -d ./backup ]; then
+    cd ./backup
+    git pull
+    cd ..
+else
+    git clone https://github.com/FoxDenHome/backup.git
+fi
