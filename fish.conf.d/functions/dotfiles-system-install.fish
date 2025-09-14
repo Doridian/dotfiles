fad5f1f9d4efea24a810809fa04750fad7230085
Doridian <git@doridian.net>
2025-09-14 10:54:12 -0700

    Allow installing multiple

--- fish.conf.d/functions/dotfiles-system-install.fish
+++ fish.conf.d/functions/dotfiles-system-install.fish
@@ -1,16 +1,26 @@
-function dotfiles-system-install -a target_file
+function dotfiles-system-install
     set -f system_dir (realpath "$_dotfiles_root_dir/../system")
     set -f target_dir '/etc'
 
-    set -f source_file "$system_dir/"(string replace -r "^$target_dir/" '' "$target_file")
+    set -f cmd_file (mktemp)
+    echo 'set -euo pipefail' > "$cmd_file"
+    echo 'set -x' >> "$cmd_file"
 
-    if not test -e "$source_file"
-        echo "Source file '$source_file' does not exist."
-        return 1
+    for target_file in $argv
+        set -f source_file "$system_dir/"(string replace -r "^$target_dir/" '' "$target_file")
+
+        if not test -e "$source_file"
+            echo "Source file '$source_file' does not exist."
+            return 1
+        end
+
+        if not test -d (dirname "$target_file")
+            echo "mkdir -p '"(dirname "$target_file")"'" >> "$cmd_file"
+        end
+        echo "cp -vaf '$source_file' '$target_file'" >> "$cmd_file"
     end
 
-    if not test -d (dirname "$target_file")
-        sudo mkdir -p (dirname "$target_file")
-    end
-    sudo cp -vaf "$source_file" "$target_file"
+    cat "$cmd_file"
+    sudo bash "$cmd_file"
+    rm -f "$cmd_file"
 end
