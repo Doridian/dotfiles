eb4fce02dc1cc8a5ef609d57cc84e7d4c6228248
Doridian <git@doridian.net>
2025-12-17 10:34:54 -0800

    HE needs some random

--- fish.conf.d/functions/he-sage-score.fish
+++ fish.conf.d/functions/he-sage-score.fish
@@ -1,12 +1,18 @@
 function he-sage-score
+    set -f tridd (random 1 65535)
+    set -f tridh (string replace '0x' '' (math -b 16 $tridd))
+
+    set -f trh "$tridd.github.io"
+    set -f tri "2a0a:8d80:0:a000::$tridh"
+
     echo '=== TRACEROUJTE ==='
-    traceroute -6 google.com
+    #traceroute -6 "$tri"
     echo '=== DIG AAAA ==='
-    dig AAAA icefox.doridian.net @8.8.8.8
+    dig AAAA "$trh" @8.8.8.8
     echo '=== DIG PTR ==='
-    dig PTR -x 2607:5300:60:7065::1 @8.8.8.8
+    dig PTR -x "$tri" @8.8.8.8
     echo '=== PING ==='
-    ping -c4 -6n google.com
+    ping -c4 -6n "$trh"
     echo '=== WHOIS ==='
-    whois '2607:5300:60:7065::1'
+    whois "$tri"
 end
