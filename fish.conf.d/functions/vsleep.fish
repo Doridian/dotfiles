2b3c6be853213f4bffdc2a3d3008970d0100d1e2
Doridian <git@doridian.net>
2026-02-05 09:15:57 -0800

    Spaces to blank out stuff

--- fish.conf.d/functions/vsleep.fish
+++ fish.conf.d/functions/vsleep.fish
@@ -1,9 +1,9 @@
 function vsleep -a duration
     set -f remaining $duration
     while test $remaining -gt 0
-        printf "\rSleeping for $remaining seconds..."
+        printf "\rSleeping for $remaining second(s)...     "
         sleep 1
         set -f remaining (math $remaining - 1)
     end
-    printf "\rSlept for $duration\n"
+    printf "\rSlept for $duration second(s)            \n"
 end
